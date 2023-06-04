6f9287c514497806dde1f3fed35eb89dbced3336
Lawrence Dunn <lawrence@comono.id>
2023-06-04 19:25:01 -0400

    Commit flake.nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A flake for building the Tealeaves website";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
+  inputs.tealeaves.url = github:dunnl/tealeaves/new-flakes;
+
+  outputs = { self, nixpkgs, tealeaves }:
+    let pkgs = import nixpkgs {
+          system = "x86_64-linux";
+        };
+        generator = pkgs.haskellPackages.callPackage
+          (import ./tealeaves-site-hakyll.nix) {};
+        generator-shell =  pkgs.haskellPackages.shellFor {
+          packages = hpkgs: [
+            generator
+          ];
+          nativeBuildInputs = [
+            pkgs.cabal-install
+          ];
+        };
+        site = pkgs.callPackage
+          (import ./tealeaves-site.nix) {
+            inherit tealeaves generator;
+          };
+    in { packages.x86_64-linux.default = generator;
+         packages.x86_64-linux.generator = generator;
+         packages.x86_64-linux.site = site;
+         devShells.x86_64-linux.default =
+           generator-shell;
+         devShells.x86_64-linux.generator =
+           generator-shell;
+         };
+}
---
+++ tealeaves-site.nix
@@ -0,0 +1,31 @@
+{ stdenv, tealeaves, generator, pkgs }:
+
+stdenv.mkDerivation {
+    name = "tealeaves-site";
+    src = ./.;
+    phases = "unpackPhase buildPhase installPhase";
+    version = "0.1";
+    buildInputs = [ generator ];
+    buildPhase = ''
+      export LOCALE_ARCHIVE="${pkgs.glibcLocales}/lib/locale/locale-archive";
+      export LANG=en_US.UTF-8
+      tealeaves-site-hakyll build
+      mkdir $out
+      cp -r _site/* $out
+      mkdir _site/coqdocs/
+      cp -r ${tealeaves.packages.x86_64-linux.default}/share/coq/${pkgs.coq.coq-version}/user-contrib/Tealeaves/html/ coqdocs/
+    '';
+    installPhase = ''
+      cp -r _site/ $out
+    '';
+    meta = {
+      description = "The contents of the Tealeaves project website";
+      longDescription = ''
+          This package contains the entire set of
+          static files that make up the Tealeaves
+          project website.
+        '';
+      homepage = "http://tealeaves.science";
+      license = pkgs.lib.licenses.mit;
+    };
+  }
